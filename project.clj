01e9c5b1240b5ce57eb8fc4933c86a34df63130c
Bronsa <brobronsa@gmail.com>
2012-12-05 16:40:23 +0100

    project: change license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject carthy/glasses "0.0.1-SNAPSHOT"
   :description "A carthy reader."
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "LGPL"
+            :url "http://www.gnu.org/copyleft/lesser.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [carthy/low "0.0.1-SNAPSHOT"]])
