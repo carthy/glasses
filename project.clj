ec3a99161663b2105c29a46352a2f27c27d40c79
Bronsa <brobronsa@gmail.com>
2012-12-16 22:16:14 +0100

    project: update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject carthy/glasses "0.0.1-SNAPSHOT"
   :description "A carthy reader."
+  :url "https://github.com/carthy/glasses"
   :license {:name "LGPL"
             :url "http://www.gnu.org/copyleft/lesser.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [carthy/low "0.0.1-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
